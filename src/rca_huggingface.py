d8e0c250598b8248986d8d23ce60436fccf50a20
Krishna0896 <103956303+Krishna0896@users.noreply.github.com>
2025-11-26 19:05:37 +0530

    Update rca_huggingface.py

--- src/rca_huggingface.py
+++ src/rca_huggingface.py
@@ -15,7 +15,7 @@ from getpass import getpass
 #hfapi_key = getpass("Enter you HuggingFace access token:")
 hfapi_key="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"
 os.environ["HF_TOKEN"] = "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"
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = 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
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 # importing HuggingFace model abstraction class from langchain
 from langchain_huggingface import HuggingFaceEndpoint
