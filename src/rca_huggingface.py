0c6c222b71628ff9c1572358740d18963f2b168d
Krishna0896 <103956303+Krishna0896@users.noreply.github.com>
2025-11-26 19:00:52 +0530

    Update rca_huggingface.py

--- src/rca_huggingface.py
+++ src/rca_huggingface.py
@@ -88,7 +88,7 @@ embedding = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
     model_kwargs={
         "device": "cpu",
-        "token": "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"
+        "token": "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"
     }
 )
 
